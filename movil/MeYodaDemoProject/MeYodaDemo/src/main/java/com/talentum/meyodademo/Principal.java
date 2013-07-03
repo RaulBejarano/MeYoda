1b668f4aa7a53f3e8f125caf9e487badea9a4a04
idiez <izan10saz@gmail.com>
2013-07-04 00:11:43 +0200

    Partes de Izan y Xandre unidas, falta añadir Navigation Activity al manifest

--- movil/MeYodaDemoProject/MeYodaDemo/src/main/java/com/talentum/meyodademo/Principal.java
+++ movil/MeYodaDemoProject/MeYodaDemo/src/main/java/com/talentum/meyodademo/Principal.java
@@ -68,7 +68,7 @@ public class Principal extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.principal, menu);
+        //getMenuInflater().inflate(R.menu.principal, menu);
 
         return true;
     }
