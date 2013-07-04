70d0486a66286875a7ca3d41b332f0961e984e07
idiez <izan10saz@gmail.com>
2013-07-04 17:22:01 +0200

    barra busqueda

--- movil/MeYodaDemoProject/MeYodaDemo/src/main/java/com/talentum/meyodademo/Mercado.java
+++ movil/MeYodaDemoProject/MeYodaDemo/src/main/java/com/talentum/meyodademo/Mercado.java
@@ -1,12 +1,15 @@
 package com.talentum.meyodademo;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 /**
  * Created by idiez on 3/07/13.
@@ -17,7 +20,15 @@ public class Mercado extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View fragment = inflater.inflate(R.layout.mercado, container, false);
+        final View fragment = inflater.inflate(R.layout.mercado, container, false);
+        ((Button) fragment.findViewById(R.id.botonBuscar)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SearchView busqueda = (SearchView) fragment.findViewById(R.id.searchView);
+                Toast toast = Toast.makeText(getActivity().getApplicationContext(), busqueda.getQuery(), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
 
         //SearchView busqueda = (SearchView) fragment.findViewById(R.id.searchView);
         //ListView lista = (ListView) fragment.findViewById(R.id.list);
