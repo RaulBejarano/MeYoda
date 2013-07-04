55cb73e6cc47fe3d009cc921e120a39348e9925a
Raul Bejarano <raulbejaranoparrilla@gmail.com>
2013-07-04 11:07:37 +0200

    asdf

--- web/mvc/controller/controller.php
+++ web/mvc/controller/controller.php
@@ -1,40 +1,42 @@
 <?php 
 	include '../model/model.php';
 
+	function lanzarQuery($sql){
+		$linkbd=mysqli("mysql.manu.juanlu.is","talentum","hypernova", "meyodadb");
+		if($linkbd->select_db("meyodadb")){
+			$result = mysqli_query(&linkbd, $sql);
+			return $result;
+		} else {
+			echo "[ERROR] Conexion BBDD"
+		}
+		
+		return null;		
+	}
+	
 	$op = $_GET["op"];
 
 	if ($op=="login"){ //OPERACION DE LOGIN
 		
-		$linkbd=mysqli("mysql.manu.juanlu.is","talentum","hypernova", "meyodadb");
-		if($linkbd->select_db("meyodadb")){
-			$sql="SELECT id FROM Usuario WHERE email = ".$_GET["email"]." AND contrasena = AES_ENCRYPT(".$_GET["contrasena"].", id)";
-		
-			$result = mysqli_query(&linkbd, $sql);
-			$userId = "";
-			while($row = mysqli_fetch_array($result)){ // Solo devolverá una linea porque email es clave unica
-				 $userId=$row['id']
-			}
-			if($userId!=""){
-				$_SESSION["userid"]=$userId;
-			}
-			
-			echo $userId;
-		}else{
-			echo "[ERROR] Conexion BBDD"
+		$sql="SELECT id FROM Usuario WHERE email = ".$_GET["email"]." AND contrasena = MD5(".$_GET["contrasena"].")";
+		$result = lanzarQuery($sql);
+		$userId = "";
+		while($row = mysqli_fetch_array($result)){ // Solo devolverá una linea porque email es clave unica
+			 $userId=$row['id']
 		}
+			
+		echo $userId;
 				
 	} else if ($op = "registro"){
-		$linkbd=mysqli("mysql.manu.juanlu.is","talentum","hypernova", "meyodadb");
-		if($linkbd->select_db("meyodadb")){
+		
 			$sql="INSERT INTO Usuario (nombre, apellidos, email, contrasena) VALUES (
 			'".$_GET['nombre']."',
 			'".$_GET['apellidos']."',
 			'".$_GET['email']."',
-			AES_ENCRYPT('".$_GET['contrasena']."', id),
+			MD5('".$_GET['contrasena']."'),
 			)";
 		
-			$result = mysqli_query(&linkbd, $sql);
-			if(result) {
+			$result = lanzarQuery(&linkbd, $sql);
+			if($result) {
 				echo "true"	
 			} else {
 				echo "false"
@@ -43,9 +45,9 @@
 		}else{
 			echo "[ERROR] Conexion BBDD"
 		}
+	} else if ($op = ""){
+		
 	}
-
-	
 
 
 ?>
