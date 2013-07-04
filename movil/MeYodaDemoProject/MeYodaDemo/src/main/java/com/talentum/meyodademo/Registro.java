f4510434c30110767235982a87251f06d33644aa
Plorenzo <palelorenzo_7@hotmail.com>
2013-07-04 12:05:41 +0200

    Corregido error formulario registro y cambio color fuente registro

--- movil/MeYodaDemoProject/MeYodaDemo/src/main/java/com/talentum/meyodademo/Registro.java
+++ movil/MeYodaDemoProject/MeYodaDemo/src/main/java/com/talentum/meyodademo/Registro.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by Pablo on 7/3/13.
  */
@@ -22,51 +25,78 @@ public class Registro extends Activity implements View.OnClickListener {
         botonEnviar.setOnClickListener(this);
 
         EditText textoNombre = (EditText) (findViewById(R.id.campoNombre));
-        textoNombre.setOnClickListener(this);
 
         EditText textoApellidos = (EditText) (findViewById(R.id.campoApellidos));
-        textoApellidos.setOnClickListener(this);
+
+        EditText textoEmail = (EditText) (findViewById(R.id.campoEmail));
 
         EditText textoClave = (EditText) (findViewById(R.id.campoContrasena));
-        textoClave.setOnClickListener(this);
 
         EditText textoConfirmClave = (EditText) (findViewById(R.id.campoConfirmacion));
-        textoConfirmClave.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
+
         String nombre = ((EditText)findViewById(R.id.campoNombre)).getText().toString();
         String apellidos = ((EditText)findViewById(R.id.campoApellidos)).getText().toString();
-        String email =  ((EditText)findViewById(R.id.email)).getText().toString();
+        String email =  ((EditText)findViewById(R.id.campoEmail)).getText().toString();
         String clave =  ((EditText)findViewById(R.id.campoContrasena)).getText().toString();
         String confirmClave = ((EditText)findViewById(R.id.campoConfirmacion)).getText().toString();
+        String claveEncriptada ;
 
 
-        if(clave.compareTo(confirmClave)!=0){
-
-            Toast.makeText(Registro.this, "Las claves no coinciden", Toast.LENGTH_LONG).show();
+        if(nombre.length()==0||apellidos.length()==0||email.length()==0||clave.length()==0){
+            Toast.makeText(Registro.this, "Error, campos vacios", Toast.LENGTH_LONG).show();
+            return;
         }
 
         if(nombre.contains(" ")){
-
             Toast.makeText(Registro.this, "Error en el nombre", Toast.LENGTH_LONG).show();
-
         }
 
         if(apellidos.split(" ").length>2){
-
             Toast.makeText(Registro.this, "Error en los apellidos", Toast.LENGTH_LONG).show();
-
         }
 
-        if(email.contains(" ") || !email.contains("@")|| !email.contains(".")){
+        if(email.contains(" ")||!email.contains("@")||!email.contains(".")){
 
             Toast.makeText(Registro.this, "Error en el email", Toast.LENGTH_LONG).show();
-
         }
+
+
+        if(clave.compareTo(confirmClave)!=0){
+            Toast.makeText(Registro.this, "Las claves no coinciden", Toast.LENGTH_LONG).show();
+        }
+
+
     }
+
+    /*public static final String md5(final String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < messageDigest.length; i++) {
+                String h = Integer.toHexString(0xFF & messageDigest[i]);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }*/
+
 
 
     public void enviar(){
